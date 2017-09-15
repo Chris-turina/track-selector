d1addb094ffe9a04b308eb3f8746a44d852c4a35
Guest User <Guest@Epicodus-10D.local>
2017-09-15 09:50:25 -0700

    add 3 more questions and scripts for their buttons but does not work

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,68 @@ $(document).ready(function(){
   // button1 function
   $("button#button1").click(function(){
     $("form#question1").submit(function(event){
-      var scoreOne = parseInt($("input:radio[name=things]:checked").val());
-      var scoreTwo = parseInt($("input:radio[name=room]:checked").val());
+      var scoreOne = parseInt($("input:radio[name=q1]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=q2]:checked").val());
+      var scoreThree = parseInt($("input:radio[name=q3]:checked").val());
+      var scoreFour = parseInt($("input:radio[name=q4]:checked").val());
+      var scoreFive = parseInt($("input:radio[name=q5]:checked").val());
       event.preventDefault();
     });
   });
-  //button2 function
+  // button2 function
   $("button#button2").click(function(){
     $("form#question2").submit(function(event){
-      var scoreOne = parseInt($("input:radio[name=things]:checked").val());
-      var scoreTwo = parseInt($("input:radio[name=room]:checked").val());
+      var scoreOne = parseInt($("input:radio[name=q1]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=q2]:checked").val());
+      var scoreThree = parseInt($("input:radio[name=q3]:checked").val());
+      var scoreFour = parseInt($("input:radio[name=q4]:checked").val());
+      var scoreFive = parseInt($("input:radio[name=q5]:checked").val());
+      event.preventDefault();
+    });
+  });
+  // button3 function
+  $("button#button3").click(function(){
+    $("form#question3").submit(function(event){
+      var scoreOne = parseInt($("input:radio[name=q1]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=q2]:checked").val());
+      var scoreThree = parseInt($("input:radio[name=q3]:checked").val());
+      var scoreFour = parseInt($("input:radio[name=q4]:checked").val());
+      var scoreFive = parseInt($("input:radio[name=q5]:checked").val());
+      event.preventDefault();
+    });
+  });
+  // button4 function
+  $("button#button4").click(function(){
+    $("form#question4").submit(function(event){
+      var scoreOne = parseInt($("input:radio[name=q1]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=q2]:checked").val());
+      var scoreThree = parseInt($("input:radio[name=q3]:checked").val());
+      var scoreFour = parseInt($("input:radio[name=q4]:checked").val());
+      var scoreFive = parseInt($("input:radio[name=q5]:checked").val());
+      event.preventDefault();
+    });
+  });
+  //button5 function
+  $("button#button5").click(function(){
+    $("form#question5").submit(function(event){
+      var scoreOne = parseInt($("input:radio[name=q1]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=q2]:checked").val());
+      var scoreThree = parseInt($("input:radio[name=q3]:checked").val());
+      var scoreFour = parseInt($("input:radio[name=q4]:checked").val());
+      var scoreFive = parseInt($("input:radio[name=q5]:checked").val());
       event.preventDefault();
 
-    var totalScore = scoreOne + scoreTwo;
-      if (totalScore === 2) {
+    //start of var if
+    var totalScore = scoreOne + scoreTwo + scoreThree + scoreFour + scoreFive;
+      if (totalScore === 5) {
         var output = "You should take the CSS-Desgin course"
         $("#css-desgin").show();
         $("output").text(output);
-      } else if (totalScore >= 3 && totalScore <= 5) {
+      } else if (totalScore >= 6 && totalScore <= 11) {
+        var output = "You should take the C# .NET course"
+        $("java-android").show();
+        $("output").text(output);
+      } else if (totalScore >= 11 && totalScore <= 15) {
         var output = "You should take the C# .NET course"
         $("#csharp-net").show();
         $("output").text(output);
