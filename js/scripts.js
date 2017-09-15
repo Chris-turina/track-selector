b98030d2f3b2840c6c80e3b7d33a8be9a8d748e2
Guest User <Guest@Epicodus-10D.local>
2017-09-15 09:04:59 -0700

    add js script for button1 and button2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 
-  
-})
+  // button1 function
+  $("button#button1").click(function(){
+    $("form#question1").submit(function(event){
+      var scoreOne = parseInt($("input:radio[name=things]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=room]:checked").val());
+      event.preventDefault();
+    });
+  });
+  //button2 function
+  $("button#button2").click(function(){
+    $("form#question2").submit(function(event){
+      var scoreOne = parseInt($("input:radio[name=things]:checked").val());
+      var scoreTwo = parseInt($("input:radio[name=room]:checked").val());
+      event.preventDefault();
+    });
+  });
+});
